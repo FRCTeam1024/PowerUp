9ff42420b88881fdc97144af7c4f21e1f5489487
Xanawatt <schneidermark2000@gmail.com>
2018-01-22 19:10:05 -0500

    drive by distance with mag encoder

--- src/org/usfirst/frc/team1024/robot/Constants.java
+++ src/org/usfirst/frc/team1024/robot/Constants.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1024.robot;
 
 public class Constants {
-	public static final double WHEEL_DIAMETER = 6.0;
+	public static final double WHEEL_DIAMETER = 6.125;
 	public static final double ENCODER_COUNTS_PER_REVOLUTION = 1024;
+	public static final double ENCODER_RATIO_TO_WHEEL = 3;
 }
--- src/org/usfirst/frc/team1024/robot/commands/DriveDistance.java
+++ src/org/usfirst/frc/team1024/robot/commands/DriveDistance.java
@@ -19,20 +19,23 @@ public class DriveDistance extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.drivetrain.resetEncoder();
     	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.drivetrain.driveDistance(distance);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.drivetrain.isMoving();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.drivetrain.stop();
     }
 
     // Called when another command which requires one or more of the same
