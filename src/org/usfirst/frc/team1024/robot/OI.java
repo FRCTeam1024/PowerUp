ca0faa36eccc343750b3fecaadee8cddf670c4bc
Xanawatt <schneidermark2000@gmail.com>
2018-02-10 18:25:25 -0500

    undo changes to OI and DriveWithJoysticks

--- src/org/usfirst/frc/team1024/robot/OI.java
+++ src/org/usfirst/frc/team1024/robot/OI.java
@@ -16,9 +16,9 @@ import edu.wpi.first.wpilibj.Joystick;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	public final Joystick lJoy = new Joystick(0);
+	/*public final Joystick lJoy = new Joystick(0);
 	public final Joystick rJoy = new Joystick(1);
-	
+	*/
 	public final Joystick intakeJoy = new Joystick(2);
 	
 	
